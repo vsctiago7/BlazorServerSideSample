9660715b62766e4d572aa4edf47c024b6a154778
Tiago Fernandes <E878@itsector.pt>
2019-06-05 18:21:46 +0100

    Renamed Backend API

--- backend/Controllers/TodoController.cs
+++ backend/Controllers/TodoController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace backend.Controllers
+namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     public class TodoController : Controller
